9e826bc34ecb48749be6e085c8dd46c75b50e2f3
Anon Cheewakarn <acheewakarn@anon-mbp13.attlocal.net>
2023-10-22 20:12:19 -0400

    init wordle project repo

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{
+      position: 'absolute', left: '50%', top: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+      <div className="row">
+        <div className="square">W</div>
+        <div className="square">O</div>
+        <div className="square">R</div>
+        <div className="square">D</div>
+        <div className="square">S</div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
