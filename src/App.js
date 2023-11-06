ec85faadb46c62d3833a7eca04eee1342fb8865e
Anon Cheewakarn <acheewakarn@gmail.com>
2023-11-06 11:52:59 -0500

    added answer validation & button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { TextInput, Text } from 'react-native';
+import { TextInput, Button } from 'react-native';
 
 export default function Game() {
 
+  const attempts = 6;
+  const answer = "HELLO";
 
 
 
@@ -34,27 +36,54 @@ function Board() {
         let updatedGuess = [...curGuess, e.key.toUpperCase()];
         setCurGuess(updatedGuess);
       }
-
-
     }
     document.addEventListener('keydown', handleKeyDown);
-
     return function cleanup() {
-
       document.removeEventListener('keydown', handleKeyDown);
     }
   }, [curGuess]);
 
+  const validateAnswer = (guess) => guess === "HELLO";
+  const handleEnter = () => {
+    console.log(curGuess.join(""));
+
+    console.log(validateAnswer(curGuess.join("")));
+  }
 
   return <div>
-    <Row word={curGuess} />
-  </div>
+    <div>
+      <Row curGuess={curGuess} />
+    </div>
+    <button disabled={curGuess.length !== 5} onClick={handleEnter}>BUTTON</button>
+  </div >
 }
 
-function Row({ word }) {
-  console.log(word);
+function Row(props) {
+  console.log();
+  //const [curGuess, setCurGuess] = useState(Array(0).fill(""));
+
+
+
+  /*  useEffect(() => {
+     function handleKeyDown(e) {
+       const inputKey = e.key;
+       if (e.code === 'Backspace') {
+         let updatedGuess = [...curGuess.slice(0, -1)];
+         setCurGuess(updatedGuess);
+ 
+       } else if (isAlpha(inputKey) && inputKey.length === 1 && curGuess.length < 5) {
+         let updatedGuess = [...curGuess, e.key.toUpperCase()];
+         setCurGuess(updatedGuess);
+       }
+     }
+     document.addEventListener('keydown', handleKeyDown);
+     return function cleanup() {
+       document.removeEventListener('keydown', handleKeyDown);
+     }
+   }, [curGuess]);
+  */
   return <div>
-    {[...Array(5)].map((e, i) => <Square key={i} value={word[i]} />)}
+    {[...Array(5)].map((e, i) => <Square key={i} value={props.curGuess[i]} />)}
 
 
   </div>
@@ -62,16 +91,23 @@ function Row({ word }) {
 
 function Square({ value }) {
   return <TextInput
-    style={{ fontFamily: "Helvetica Neue", borderWidth: 1, borderColor: '#999', width: 62, height: 62, marginRight: 5, marginTop: 5, padding: 0, fontSize: "2rem", fontWeight: "bold", textAlign: 'center' }}
+    disabled={true}
+    style={{
+      fontFamily: "Helvetica Neue",
+      borderWidth: 1, borderColor: '#999',
+      width: 62,
+      height: 62,
+      marginRight: 5,
+      marginTop: 5,
+      padding: 0,
+      fontSize: "2rem",
+      fontWeight: "bold",
+      textAlign: 'center'
+    }}
     value={value === undefined ? "" : value} />
-  /*   return <TextInput
-      disabled={true}
-      className="square"
-      maxLength={1}
-      value={value}
-    /> */
 }
 
+// modified snippet from https://stackoverflow.com/questions/4434076/best-way-to-alphanumeric-check-in-javascript
 function isAlpha(str) {
   var code, i, len;
 
@@ -83,7 +119,6 @@ function isAlpha(str) {
       return false;
     }
   }
-  console.log(str + " is alpha");
   return true;
 };
 
